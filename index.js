464bb82dff9822497683d989bf03db7435f3b809
BilalKeb <90609831+BilalKeb@users.noreply.github.com>
2021-10-05 20:37:48 +0200

    SHIFUMI-V2

--- index.js
+++ index.js
@@ -11,9 +11,31 @@ imageFeuille.setAttribute("src", "https://www.ecole-occidentale-meditation.com/w
 var imageCis = document.getElementById("image-cis");
 imageCis.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRHnPufKRrNBF2rJ7L5E60ARY4SoyoWDDyoBAhEB4-PaVlYOWnRMFEeTN1ZA5XsnmNZUg&usqp=CAU");
 
+  var bouton_pierre=document.getElementById('bouton_pierre')
+  var bouton_feuille=document.getElementById('bouton_feuille')
+  var bouton_cis=document.getElementById('bouton_cis')
+
+  var PointIa=0
+  var PointJoueur=0
+  var CounterRound= 0
+
+
+
+ 
 
 function pierre (){
+  if(CounterRound>4){
+    if(PointJoueur < PointIa ){
+      alert("l'IA a gagné !!!!")
+    }else {
+      alert("Vous avez gagné !!!!")
+    }
+    alert('la partie est terminée')
+    bouton_pierre.setAttribute("disabled", "disabled")
+    return
+  }
   var aleatoire= Math.floor(Math.random() * 3);
+  // aleatoire contient soit 0, soit 1, soit 2
   if(aleatoire==0){
     image.src="https://resize-parismatch.lanmedia.fr/f/webp/r/392/img/var/news/storage/images/paris-match/actu/sciences/les-tresors-d-un-chasseur-de-meteorites-aux-encheres-1605299/c11/25998027-1-fre-FR/C11.jpg"
   }else if(aleatoire==1){
@@ -23,11 +45,15 @@ function pierre (){
   }
 
   if(image.src=="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRHnPufKRrNBF2rJ7L5E60ARY4SoyoWDDyoBAhEB4-PaVlYOWnRMFEeTN1ZA5XsnmNZUg&usqp=CAU"){
-    alert("on a gagné")
+    alert("on a gagné ce round")
+    PointJoueur++;
+    CounterRound++;
   }
 
   if(image.src=="https://www.ecole-occidentale-meditation.com/wp-content/uploads/2016/04/page+blanche-300x400.jpg"){
-    alert("on a perdu")
+    alert("on a perdu ce round")
+    PointIa++;
+    CounterRound++;
   }
   
 }
@@ -37,6 +63,16 @@ function pierre (){
 
 
 function feuille(){
+  if(CounterRound>4){
+    if(PointJoueur < PointIa ){
+      alert("l'IA a gagné !!!!")
+    }else {
+      alert("Vous avez gagné !!!!")
+    }
+    alert('la partie est terminée')
+    bouton_feuille.setAttribute("disabled", "disabled")
+    return
+  }
   var aleatoire= Math.floor(Math.random() * 3);
   if(aleatoire==0){
     image.src="https://resize-parismatch.lanmedia.fr/f/webp/r/392/img/var/news/storage/images/paris-match/actu/sciences/les-tresors-d-un-chasseur-de-meteorites-aux-encheres-1605299/c11/25998027-1-fre-FR/C11.jpg"
@@ -47,16 +83,31 @@ function feuille(){
   }
 
   if(image.src=="https://resize-parismatch.lanmedia.fr/f/webp/r/392/img/var/news/storage/images/paris-match/actu/sciences/les-tresors-d-un-chasseur-de-meteorites-aux-encheres-1605299/c11/25998027-1-fre-FR/C11.jpg"){
-    alert("on a gagné")
+    alert("on a gagné ce round")
+    PointJoueur++;
+    CounterRound++;
   }
   if(image.src=="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRHnPufKRrNBF2rJ7L5E60ARY4SoyoWDDyoBAhEB4-PaVlYOWnRMFEeTN1ZA5XsnmNZUg&usqp=CAU"){
-    alert("on a perdu")
+    alert("on a perdu ce round")
+    PointIa++;
+    CounterRound++;
   }
+  alert("le score du joueur est : "+PointJoueur+"    et le score de l'IA est : "+PointIa+"\n on est dans le round : "+CounterRound)
 }
 
 
 function Cis(){
-  var aleatoire= Math.floor(Math.random() * 3);
+  if(CounterRound>4){
+    if(PointJoueur < PointIa ){
+      alert("l'IA a gagné !!!!")
+    }else {
+      alert("Vous avez gagné !!!!")
+    }
+    alert('la partie est terminée')
+    bouton_cis.setAttribute("disabled", "disabled")
+    return
+  }
+  var aleatoire= Math.floor(Math.random() * 9);
   if(aleatoire==0){
     image.src="https://resize-parismatch.lanmedia.fr/f/webp/r/392/img/var/news/storage/images/paris-match/actu/sciences/les-tresors-d-un-chasseur-de-meteorites-aux-encheres-1605299/c11/25998027-1-fre-FR/C11.jpg"
   }else if(aleatoire==1){
@@ -65,11 +116,34 @@ function Cis(){
     image.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRHnPufKRrNBF2rJ7L5E60ARY4SoyoWDDyoBAhEB4-PaVlYOWnRMFEeTN1ZA5XsnmNZUg&usqp=CAU"
   }
   if(image.src=="https://www.ecole-occidentale-meditation.com/wp-content/uploads/2016/04/page+blanche-300x400.jpg"){
-    alert("on a gagné")
+    alert("on a gagné ce round")
+    PointJoueur++;
+    CounterRound++;
   }
   if(image.src=="https://resize-parismatch.lanmedia.fr/f/webp/r/392/img/var/news/storage/images/paris-match/actu/sciences/les-tresors-d-un-chasseur-de-meteorites-aux-encheres-1605299/c11/25998027-1-fre-FR/C11.jpg"){
-    alert("on a perdu")
+    alert("on a perdu ce round")
+    PointIa++;
+    CounterRound++;
   }
+  
+} 
 
-}
-    
+
+
+// counterRound=counterRound +1 
+
+// if (counter===5 && PointJoeur> PointIa){
+//   console.log(you win)
+
+// {else if (counterRound===5 && PointIa> pointsJoueur){
+  // console.log (you loose)
+
+
+
+
+// }else if (counterRound ===5 && PointIa){
+//     console.log(you win)
+// }
+
+
+
